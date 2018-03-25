ad5678ef5a51e7e03406e83ae6baeeb918682244
Katsuhi Yoshida <archer0729@icloud.com>
2018-03-25 08:41:48 +0000

    リファクタリング、バグつぶし

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def message_text(event):
     lines = event.message.text.split("\n")
     msgs = []
     for line in lines:
-        msg = roll_message(line)
+        msg = make_message(line)
         if msg is not  None:
             msgs.append(msg)
 
@@ -62,80 +62,81 @@ def message_text(event):
             TextSendMessage(text=msg)
         )
 
-def roll_message(roll_str):
-    if roll_str is None and len(roll_str) == 0 and not isinstance(roll_str, str):
+def make_message(text):
+    if text is None and len(text) == 0 and not isinstance(text, str):
         return None
-    roll = mojimoji.zen_to_han(roll_str).lower()
-    if roll.startswith("dcoc"):
-        msg = roll_coc(roll)
-    elif "<" in roll or ">" in roll or "=" in roll:
-        msg = hantei_roll(roll)
-    else:
-        msg = dice_roll(roll)
-    return msg
+    lower_text = mojimoji.zen_to_han(text).lower()
 
-def roll_coc(roll):
-    obj_point = roll[4:]
-    print(obj_point)
-    roll_val = rolling(1, 100)[0]
-    if obj_point:
-        point = int(obj_point)
-        msg = result_coc(roll_val, point)
-    else:
-        return "1d100 = %d" % roll_val
-    return "1d100 = %d <= %s -> %s" % (roll_val, obj_point, msg)
+    if lower_text.startswith("dcoc "):
+        return roll_coc(lower_text)
+    #if lower_text.startswith("dsf "):
+    #    return roll_sf(lower_text)
 
-CRITICAL_MESSAGE = {
-    True: "クリティカル",
-    False: "失敗"
-}
-FUMBLE_MESSAGE = {
-    True: "成功",
-    False: "ファンブル"
-}
-RESULT_MESSAGE = {
-    True: "成功",
-    False: "失敗"
-}
-def result_coc(val, obj):
-    msgs = []
-    is_success = val <= obj
-    if val <= 5:
-        msgs.append(CRITICAL_MESSAGE[is_success])
-    if val <= (obj / 5):
-        msgs.append("スペシャル")
-    if val >= 96:
-        msgs.append(FUMBLE_MESSAGE[is_success])
-    if len(msgs) == 0:
-        return RESULT_MESSAGE[is_success]
-    return "/".join(msgs)
+    dice_str, opr, opt = parse_text_to_dice_str(lower_text)
+    if dice_str is None:
+        return None
+    if opr is not None:
+        dice_str, res, opr, is_succ = hantei_roll(dice_str, opr)
+        return "%s = %s = %d %s -> %s" % (dice_str, str(res), sum(res), opr, is_succ)
+    dice_str, res = roll_dice_str(dice_str)
+    return "%s = %s = %d" % (dice_str, str(res), sum(res))
 
-def hantei_roll(roll):
-    roll_dice, opr, obj_point = re.sub(" *([^0-9]*[<>=][^0-9]*) *", "\t\\1\t", roll).split("\t")
-    deme, deme_sum = dice_roll(roll_dice).split(" = ")[1:]
-    is_success = success(int(deme_sum), int(obj_point), opr)
-    msg = "%s = %s = %s %s %s %s" % (roll_dice, str(deme), deme_sum, opr, obj_point, is_success)
-    return msg
+dice_pattern = re.compile("[0-9]*d[0-9]*")
+operation_pattern = re.compile("[<>=][<>=]* *[0-9][0-9]*")
+opr_split_pattern = re.compile("([<>=]) *([0-9])")
+def parse_text_to_dice_str(text):
+    # ダイスの存在確認
+    match = dice_pattern.match(text.strip())
+    if match is None: # 存在しないときは無視する
+        return None
+    dice_str = match.group()
+    option = text[match.end():].strip()
 
-def dice_roll(roll):
-    num, dice = split_dice(roll)
+    # 判定器が存在するかの確認
+    match = operation_pattern.match(option)
+    if match is None:
+        return dice_str, None, option
+    operater = match.group()
+    add_str = option[match.end():]
+    return dice_str, operater, add_str
+
+def split_opr_and_obj(opr):
+    return opr_split_pattern.sub("\\1 \\2", opr).split()
+
+def hantei_roll(dice_str, opr):
+    opr, obj = split_opr_and_obj(opr)
+    dice_str, roll_result = roll_dice_str(dice_str)
+    is_success = success(sum(roll_result), int(obj), opr)
+    return dice_str, roll_result, "%s %s" % (opr, obj), is_success
+    #msg = "%s = %s = %s %s %s %s" % (dice_str, str(deme), deme_sum, opr, obj_point, is_success)
+    #return msg
+
+def roll_dice_str(dice_str):
+    num, dice = split_dice(dice_str)
     if num is None:
         return None
-    result = rolling(num, dice)
-    return "%s = %s = %d" % (roll, str(result), sum(result))
+    return "%sd%s" % (num, dice), roll_dices(int(num), int(dice))
 
-def split_dice(msg):
-    split = msg.split("d")
-    if len(split) != 2 or not split[0].isdigit() or not split[1].isdigit():
-        return None, None
+def split_dice(dice_str):
+    if dice_str is None:
+        return None
+    split = dice_str.split("d")
+    if len(split) != 2:
+        return None
+    if split[0] == "" or not split[0].isdigit():
+        split[0] = "1"
+    if split[1] == "" or not split[1].isdigit():
+        split[1] = "6"
     return split
 
-def rolling(num, dice):
-    return [random.randint(1,int(dice)) for _ in range(int(num))]
+def roll_dices(num, dice):
+    return [roll_dice(dice) for _ in range(num)]
+
+def roll_dice(dice):
+    return random.randint(1, dice)
 
 def success(deme, obj_point, operater):
     operater = operater.strip()
-    print(operater)
     results = {True: "成功", False: "失敗", None: "不明"}
     is_success = None
     if operater == "<=":
@@ -167,6 +168,42 @@ def get_user_name(source):
     else:
         user_name = ""
     return user_name
+
+def roll_coc(roll):
+    obj_point = roll.split()[1]
+    roll_val = roll_dices(1, 100)[0]
+
+    if obj_point.isdigit():
+        point = int(obj_point)
+        msg = result_coc(roll_val, point)
+    else:
+        return "1d100 = %d" % roll_val
+    return "1d100 = %d <= %s -> %s" % (roll_val, obj_point, msg)
+
+CRITICAL_MESSAGE = {
+    True: "クリティカル",
+    False: "失敗"
+}
+FUMBLE_MESSAGE = {
+    True: "成功",
+    False: "ファンブル"
+}
+RESULT_MESSAGE = {
+    True: "成功",
+    False: "失敗"
+}
+def result_coc(val, obj):
+    msgs = []
+    is_success = val <= obj
+    if val <= 5:
+        msgs.append(CRITICAL_MESSAGE[is_success])
+    if val <= (obj / 5):
+        msgs.append("スペシャル")
+    if val >= 96:
+        msgs.append(FUMBLE_MESSAGE[is_success])
+    if len(msgs) == 0:
+        return RESULT_MESSAGE[is_success]
+    return "/".join(msgs)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
